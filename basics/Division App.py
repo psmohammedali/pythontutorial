4a749f19d932c640301cfcbc3abbc4ea11979f2f
Mohammed Ali P S <48435733+psmohammedali@users.noreply.github.com>
2020-08-09 16:55:07 +0530

    Add files via upload

---
+++ basics/Division App.py
@@ -0,0 +1,23 @@
+print("Welcome to Ali page of Division")
+
+print("Enter your first Number")
+up = int(input())
+
+print("Enter your Divider Number")
+divider = int(input())
+
+print(" You have entered the values as")
+print(up)
+print(divider)
+
+operation = up // divider
+print(operation)
+reminder = up % divider
+print(reminder)
+
+
+print("Details:")
+print(str(up)+ " is divided by "+str(divider) + " and gets the reminder "+str(reminder))
+
+
+
---
+++ basics/dictionary.py
@@ -0,0 +1,15 @@
+myDict = {
+    # key (string/number, ):value (string, values, list, tuple)
+     87 : "Mohammed Ali",
+    311 : "Arul",
+    289 : "Srienath",
+}
+print(myDict.keys())
+print(myDict.values())
+print("")
+print(myDict[87])
+
+
+# dictionary : {} , Liberity to set keys
+#  list :[] , like a arrary, where you can update values with the help of index
+# tuple : (), permanent array, where you can't update values
